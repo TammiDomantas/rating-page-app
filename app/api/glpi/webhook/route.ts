cd8e00ff1298a0c1dce7f0934ec0152e110d0a39
Tammi <domantas.tammi@sa.stud.vu.lt>
2026-04-22 13:23:19 +0300

    Remove API get request, get item.user_editor as technician instead

--- app/api/glpi/webhook/route.ts
+++ app/api/glpi/webhook/route.ts
@@ -1,66 +1,25 @@
 import { NextResponse } from "next/server";
 
-
-async function getAssignedTechnician(ticketId: string) {
-  // 1. Start GLPI API session
-  const sessionRes = await fetch(
-    `${process.env.GLPI_URL}/apirest.php/initSession`,
-    {
-      method: "GET",
-      headers: {
-        "App-Token": process.env.GLPI_APP_TOKEN!,
-        Authorization: `user_token ${process.env.GLPI_USER_TOKEN!}`,
-      },
-    }
-  );
-
-  const session = await sessionRes.json();
-
-  // Request users linked to this ticket
-  const usersRes = await fetch(
-    `${process.env.GLPI_URL}/apirest.php/Ticket/${ticketId}/Ticket_User`,
-    {
-      method: "GET",
-      headers: {
-        "App-Token": process.env.GLPI_APP_TOKEN!,
-        "Session-Token": session.session_token,
-      },
-    }
-  );
-
-  const users = await usersRes.json();
-
-  // find technician (type 2 = assigned)
-  return Array.isArray(users)
-    ? users.find((user: any) => user.type === 2)
-    : null;
-}
-
 export async function POST(req: Request) {
   try {
-    // Read raw (for testing)
+    // get and output raw body (for testing)
     const rawBody = await req.text();
 
     console.log("RAW WEBHOOK BODY:");
     console.log(rawBody);
 
-    // Convert payload string into JSON object
+    // extract json
     const body = JSON.parse(rawBody);
 
-    // Extract fields
     const event = body.event;
     const ticket = body.ticket;
 
     const ticketId = ticket?.id;
     const status = ticket?.status;
     const requester = ticket?.requester;
+    const technician = ticket?.technician;
 
-    // get technician from glpi api
-    const technician = ticketId
-      ? await getAssignedTechnician(ticketId)
-      : null;
-
-    // Log 
+    // log
     console.log("EXTRACTED FIELDS:");
     console.log({
       event,
@@ -69,9 +28,8 @@ export async function POST(req: Request) {
       requester,
       technician,
     });
-
+    
     return NextResponse.json({ ok: true });
-
   } catch (err) {
     console.error("Webhook error:", err);
 
