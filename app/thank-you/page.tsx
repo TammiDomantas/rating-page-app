78aaea979bf9eae17efb54578d265cb5d46a06ce
Tammi <domantas.tammi@sa.stud.vu.lt>
2026-05-05 11:02:56 +0300

    add return button to thank-you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 export default function ThankYouPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+      <div className="bg-white p-8 rounded-2xl shadow w-full max-w-md text-center space-y-4">
       <h1 className="text-2xl text-gray-600 font-bold">
         Ačiū už įvertinimą!
       </h1>
+      <Link
+        href="/"
+        className="block w-full bg-blue-600 text-white text-center py-3 rounded-xl hover:bg-blue-700 transition"
+        >
+        Grįžti atgal
+      </Link>
+      </div>
     </main>
   );
 }
