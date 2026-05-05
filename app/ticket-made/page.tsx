ebbb1d4cc504796f96036c15dc81628f2b553fea
Tammi <domantas.tammi@sa.stud.vu.lt>
2026-05-05 10:40:25 +0300

    add ticket-made button to return to start

--- app/ticket-made/page.tsx
+++ app/ticket-made/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 export default function TicketMadePage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
       <h1 className="text-2xl text-gray-600 font-bold">
         Užklausa sukurta sėkmingai!
       </h1>
+      <Link
+        href="/"
+        className="block w-full bg-blue-600 text-white text-center py-3 rounded-xl hover:bg-blue-700 transition"
+        >
+        Grįžti atgal
+      </Link>
     </main>
   );
 }
