94a30004f336e197ed30b098463c961ebeef2940
Tammi <domantas.tammi@sa.stud.vu.lt>
2026-05-05 10:43:16 +0300

    fix formatting

--- app/ticket-made/page.tsx
+++ app/ticket-made/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 export default function TicketMadePage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+     <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+      <div className="bg-white p-8 rounded-2xl shadow w-full max-w-md text-center space-y-4">
       <h1 className="text-2xl text-gray-600 font-bold">
         Užklausa sukurta sėkmingai!
       </h1>
@@ -11,6 +12,7 @@ export default function TicketMadePage() {
         >
         Grįžti atgal
       </Link>
+      </div>
     </main>
   );
 }
